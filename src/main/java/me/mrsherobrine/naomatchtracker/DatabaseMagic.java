367216f03b50a6fb2b7ad6a8422b2d128f9b6232
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2021-07-16 23:38:41 +0200

    get started on the database magic

--- src/main/java/me/mrsherobrine/naomatchtracker/DatabaseMagic.java
+++ src/main/java/me/mrsherobrine/naomatchtracker/DatabaseMagic.java
@@ -1,4 +1,48 @@
 package me.mrsherobrine.naomatchtracker;
 
+import java.sql.PreparedStatement;
+import java.util.regex.Pattern;
+
+/*
+    this program is made by github.com/mrsherobrine
+    please read the licensing information in the readme
+    thanks for using my tool
+ */
+
 public class DatabaseMagic {
+    public static void databaseMagicInsert(String input) throws Exception {
+
+        if (!Pattern.matches("[0-9]\\w:[0-9]\\w",input)) {
+            System.out.println(input + " errored!");
+            throw new Exception("Bad format!");
+        } else {
+            System.out.println(input + " insert from dbm");
+            //PreparedStatement insertQuery = prepareStatement("INSERT INTO time(timespent) VALUES ('?')");
+        }
+    }
+
+
+    public static String databaseMagicAverage(String input) throws Exception {
+
+        if (!Pattern.matches("[0-9]\\w:[0-9]\\w",input)) {
+            System.out.println(input);
+            throw new Exception("Bad format!");
+        } else {
+            System.out.println(input + " avg from dbm");
+            //PreparedStatement insertQuery = prepareStatement("INSERT INTO time(timespent) VALUES ('?')");
+        }
+        return input + " avg";
+    }
+
+    public static String databaseMagicTotal(String input) throws Exception {
+        if (!Pattern.matches("[0-9]\\w:[0-9]\\w",input)) {
+            System.out.println(input);
+            throw new Exception("Bad format!");
+        } else {
+            System.out.println(input + " total from dbm");
+            //PreparedStatement insertQuery = prepareStatement("INSERT INTO time(timespent) VALUES ('?')");
+        }
+        return input + " total";
+    }
+
 }
--- src/main/java/me/mrsherobrine/naomatchtracker/Main.java
+++ src/main/java/me/mrsherobrine/naomatchtracker/Main.java
@@ -1,53 +1,96 @@
 package me.mrsherobrine.naomatchtracker;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /*
     this program is made by github.com/mrsherobrine
     please read the licensing information in the readme
     thanks for using my tool
  */
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+
 public class Main {
 
-    public JTextField textField = new JTextField("mm:ss");
+    public JTextField textField = new JTextField("");
     public JFrame window = new JFrame("naoMatchTracker");
     public JButton button = new JButton("Insert");
-    public JPanel panel = new JPanel();
+    public JLabel howtoLabel = new JLabel("Input your match time in mm:ss format here ^");
+    public JLabel averageLabel = new JLabel("Average match duration: ");
+    public JLabel totalLabel = new JLabel("Total time spent in game: ");
+    public Container cp = window.getContentPane();
+    public String averageDurationToStopDeletionBecauseIAmLazy = "Average match duration: ";
+    public String totalDurationToStopWipingItEveryButtonClick = "Total time spent in game: ";
+    public String h = "";
 
-    public Main() {
-        Dimension a = new Dimension();
-        a.setSize(400, 400);
-        System.getProperty("user.dir");
-        Image icon = Toolkit.getDefaultToolkit().getImage("files/icon.png");
+    public Main() throws IOException {
+        window.setSize(500,400);
+        Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation(((a.width - window.getSize().width)/2), ((a.height - window.getSize().height)/2));
 
-        window.setMinimumSize(a);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        URL iconLocation = this.getClass().getResource("/files/icon.png");
+        Image icon = ImageIO.read(iconLocation);
         window.setIconImage(icon);
-        //window.setLayout(new GridBagLayout());
+        window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-        button.setBounds(0,0, 200, 50);
+        button.setBounds(48, 256, 153, 33);
         button.addActionListener(this::insertButtonActionPerformed);
+        button.setBackground(new Color(0x5f5f5f));
+        button.setForeground(Color.white);
+        button.setBorder(BorderFactory.createLineBorder(Color.white, 1, true));
+        button.setFocusable(false);
 
-        textField.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-        textField.setSize(300, 120);
+        textField.setBounds(240, 256, 145, 33);
+        textField.setBackground(new Color(0x212121));
+        textField.setForeground(Color.white);
+        averageLabel.setBounds(48, 72, 400, 57);
+        averageLabel.setBackground(new Color(0x212121));
+        averageLabel.setForeground(Color.white);
+        totalLabel.setBounds(48, 152, 400, 65);
+        totalLabel.setBackground(new Color(0x212121));
+        totalLabel.setForeground(Color.white);
+        howtoLabel.setBounds(48, 304, 400, 33);
+        howtoLabel.setBackground(new Color(0x212121));
+        howtoLabel.setForeground(Color.white);
 
-        panel.setSize(400,400);
-        panel.add(button);
-        panel.add(textField);
-        window.add(panel/*, new GridBagConstraints()*/);
-        window.pack();
+        cp.setLayout(null);
+        cp.setBackground(new Color(0x212121));
+        cp.add(button);
+        cp.add(textField);
+        cp.add(averageLabel);
+        cp.add(totalLabel);
+        cp.add(howtoLabel);
         window.setVisible(true);
     }
 
     public static void main(String[] args) {
-        new Main();
+        try {
+            new Main();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
     }
 
-    public void insertButtonActionPerformed(ActionEvent e) {
-        textField.setText("lol u dum");
+    public void insertButtonActionPerformed(ActionEvent e){
+        try {
+            h = textField.getText();
+            if (!h.contains(":")) throw new Error();
+            DatabaseMagic.databaseMagicInsert(h);
+            System.out.println(DatabaseMagic.databaseMagicTotal(h) + " from main");
+            System.out.println(DatabaseMagic.databaseMagicAverage(h) + " from main");
+            textField.setText("");
+            howtoLabel.setText("Input your match time in mm:ss format here ^");
+            howtoLabel.setForeground(Color.white);
+            textField.setBackground(new Color(0x212121));
+        } catch (Exception | Error whoops) {
+            howtoLabel.setText("That's not in mm:ss format!");
+            howtoLabel.setForeground(new Color(0xdf1310));
+            textField.setBackground(Color.red);
+            textField.setText("");
+        }
     }
+
 }
